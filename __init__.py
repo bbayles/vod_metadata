b21f540ca7b53aeb569f7034de41da0dc4dd7b03
Bo Bayles <bbayles@gmail.com>
2014-02-05 21:25:01 -0600

    Read the configuration file upon import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
+import os.path
 from vod_metadata.md5_checksum import *
 from vod_metadata.media_info import *
 from vod_metadata.parse_config import *
 from vod_metadata.VodPackage import *
+
+_script_path = os.path.abspath(__file__)
+_script_path = os.path.split(_script_path)[0]
+config_path = os.path.join(_script_path, "template_values.ini")
 
 (extensions,
  MediaInfo_path,
@@ -10,4 +15,4 @@ from vod_metadata.VodPackage import *
  prefix,
  title_category,
  provider,
- ecn_2009) = parse_config("./template_values.ini")
+ ecn_2009) = parse_config(config_path)
