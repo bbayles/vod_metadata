e00f5ae1b53b5a0739f4639046bec4fa47e0b798
Bo Bayles <bbayles@gmail.com>
2014-05-30 08:26:12 -0500

    Pretty print when lxml is not available

--- xml_helper.py
+++ xml_helper.py
@@ -13,6 +13,23 @@ except ImportError:
   import xml.etree.ElementTree as etree
   lxml = False
 
+# Taken from Fredrik Lundh's effbot.org:
+# http://effbot.org/zone/element-lib.htm#prettyprint
+def indent(elem, level=0):
+  i = "\n" + level*"  "
+  if len(elem):
+    if not elem.text or not elem.text.strip():
+      elem.text = i + "  "
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+    for elem in elem:
+      indent(elem, level + 1)
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+  else:
+    if level and (not elem.tail or not elem.tail.strip()):
+      elem.tail = i
+
 def tobytes(ADI):
   doctype = b'<!DOCTYPE ADI SYSTEM "ADI.DTD">'
   if lxml:
@@ -23,6 +40,7 @@ def tobytes(ADI):
                           pretty_print=True)
   else:
     declaration = b'<?xml version="1.0" encoding="utf-8"?>'
+    indent(ADI)
     elements = etree.tostring(ADI, encoding="utf-8")
     
     return b''.join((declaration, doctype, elements))
