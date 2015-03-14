0d74a78939edf4320eafe5b3cf2b8abdc98200e1
Bo Bayles <bbayles@gmail.com>
2015-03-14 12:13:36 -0500

    Specify MediaInfo path on the command line

--- vod_metadata/__main__.py
+++ vod_metadata/__main__.py
@@ -3,21 +3,32 @@
 # See http://github.com/bbayles/vod_metadata for documentation and license
 from __future__ import print_function
 
+from argparse import ArgumentParser
 from io import open
-import os
+from os import getcwd, listdir
+from os.path import splitext
 
 from vod_metadata import config_path
 from vod_metadata.config_read import parse_config
 from vod_metadata.md_gen import generate_metadata
 
 
-if __name__ == "__main__":
-    # Retrieve the user's configuration
-    vod_config = parse_config(config_path)
+# Accept arguments from the command line
+parser = ArgumentParser()
+parser.add_argument(
+    '--mediainfo-path', help='Specify the path to the MediaInfo executable'
+)
+args = parser.parse_args()
 
-    for file_path in os.listdir(os.getcwd()):
+if __name__ == "__main__":
+    # Retrieve the user's configuration, overriding with command line arguments
+    vod_config = parse_config(config_path)
+    if args.mediainfo_path is not None:
+        vod_config = vod_config._replace(mediainfo_path=args.mediainfo_path)
+
+    for file_path in listdir(getcwd()):
         # Only process movie files
-        file_name, file_ext = os.path.splitext(file_path)
+        file_name, file_ext = splitext(file_path)
         if file_ext not in vod_config.extensions:
             continue
 
