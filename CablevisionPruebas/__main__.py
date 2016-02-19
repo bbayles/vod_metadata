0b06c3e5f4ad712f4b8f0f2b26a04b263c92a078
Mastromarino <nmastromarino@cablevision.com.ar>
2016-02-19 16:56:28 -0300

    Probando Interface Grafica!

---
+++ CablevisionPruebas/__main__.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+
+class MyFrame(Frame):
+    def __init__(self):
+        Frame.__init__(self)
+        self.master.title("Example")
+        self.master.rowconfigure(5, weight=1)
+        self.master.columnconfigure(5, weight=1)
+        self.grid(sticky=W+E+N+S)
+
+        self.button = Button(self, text="Browse", command=self.load_file, width=10)
+        self.button.grid(row=1, column=0, sticky=W)
+
+    def load_file(self):
+        fname = askopenfilename(filetypes=(("Template files", "*.tplate"),
+                                           ("HTML files", "*.html;*.htm"),
+                                           ("All files", "*.*") ))
+        if fname:
+            try:
+                print("""here it comes: {}""", fname)
+            except:                     # <- naked except is a bad idea
+                showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+            return
+
+
+if __name__ == "__main__":
+    MyFrame().mainloop()
